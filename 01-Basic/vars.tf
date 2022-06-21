321ebf3ab356a6371274644aa6f1923dd405a513
nishneo12 <nishant.bhatt.27@gmail.com>
2022-06-21 13:31:56 +0530

    Update vars.tf

--- 01-Basic/vars.tf
+++ 01-Basic/vars.tf
@@ -3,6 +3,25 @@ variable "resource_group_name_prefix" {
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
+variable "resource_group_suffix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+variable "vnet_suffix" {
+  default       = "vNet"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "subNet_suffix" {
+  default       = "subNet"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "publicIP_suffix" {
+  default       = "publicIP"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
 variable "resource_group_location" {
   default       = "eastus"
   description   = "Location of the resource group."
