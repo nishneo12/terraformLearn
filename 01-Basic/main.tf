0a9755cf37d9f476c971485d6253ceb6717ee764
nishneo12 <nishant.bhatt.27@gmail.com>
2022-06-21 11:57:49 +0530

    Update main.tf

--- 01-Basic/main.tf
+++ 01-Basic/main.tf
@@ -1,1 +1,9 @@
+provider "azurerm" {
+   version = "=1.35.0"
+}
 
+#create a resource group
+resource "azurerm_resource_group" "demo" {
+  name     = "first-steps-demo"
+  location = var.location
+}
