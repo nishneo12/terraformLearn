21a93139e92c9aaf7eb3bb95b987525b8d5c2256
nishneo12 <nishant.bhatt.27@gmail.com>
2022-06-21 12:35:57 +0530

    Update vars.tf

--- 01-Basic/vars.tf
+++ 01-Basic/vars.tf
@@ -1,17 +1,9 @@
-variable location
-{
-  type= string
-  default = "westeurope"
+variable "resource_group_name_prefix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable prefix
-{
-  type = string
-  default = "demo"
-}
-
-variable ssh-source-address
-{
- type = string
-  default = "*"
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
 }
